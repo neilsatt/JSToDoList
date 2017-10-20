dd1f47931a722d5a2b0b378db122c59d99f8024a
neilsatt <neilsatt@gmail.com>
2017-10-19 20:52:09 -0600

    updates including local storage comments

--- library_storage.js
+++ library_storage.js
@@ -1,18 +1,19 @@
+// Local Storage Utility Library
 "use strict";
 var getStorage = function(key) {
     //get string from storage or an empty string if nothing in storage
     var storage = localStorage.getItem(key) || "";
     if (storage === "") { 
-        return []; 
+        return []; // return empty if nothing in local storage
     } else {
-        return storage.split("|");
+        return storage.split("|"); // return array 
     }
 };
 
 var setStorage = function(key, arr) {
-    if (Array.isArray(arr)) {
-        var storageString = arr.join("|"); 
-        localStorage.setItem(key, storageString);
+    if (Array.isArray(arr)) { // confirm 2nd param is an array
+        var storageString = arr.join("|"); // separate all of the tasks with a pipe symbol
+        localStorage.setItem(key, storageString); // add to local storage
     }
 };
 
